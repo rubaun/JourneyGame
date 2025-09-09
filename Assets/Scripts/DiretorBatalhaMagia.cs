81997b602255b6384889ceeaa0dd86f52095ad24
Ruben Prado <43066684+rubaun@users.noreply.github.com>
2025-09-09 17:55:52 -0300

    Inicio do Caminho e Sistema de Batalha Atualizado

---
+++ Assets/Scripts/DiretorBatalhaMagia.cs
@@ -0,0 +1,260 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DiretorBatalhaMagia : MonoBehaviour
+{
+
+    [Header("Timer UI")]
+    [SerializeField] int tempoRoundPlayer = 20;
+    [SerializeField] TextMeshProUGUI indicadorTempo;
+    [Header("Player UI")]
+    [SerializeField] Mago player;
+    [SerializeField] TextMeshProUGUI nomePlayer;
+    [SerializeField] TextMeshProUGUI vidaPlayer;
+    [SerializeField] Slider manaPlayer;
+    [SerializeField] TextMeshProUGUI informativo;
+    [SerializeField] TextMeshProUGUI indicadorEspecial;
+    [SerializeField] Button botaoEspecial;
+    [SerializeField] Button botaoAtaque;
+    [SerializeField] Button botaoDefesa;
+    [Header("Inimigo UI")]
+    [SerializeField] Mago inimigo;
+    [SerializeField] TextMeshProUGUI nomeInimigo;
+    [SerializeField] TextMeshProUGUI vidaInimigo;
+    [SerializeField] Slider manaInimigo;
+
+    string turno = "Player";
+    bool verificadorDeTurno = true;
+    bool verificadorDoContador = true;
+    Coroutine contadorCoroutine;
+    int contador;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        vidaPlayer = GameObject.Find("VidaPlayer").GetComponent<TextMeshProUGUI>();
+        vidaPlayer.text = player.GetVida().ToString();
+        vidaInimigo = GameObject.Find("VidaInimigo").GetComponent<TextMeshProUGUI>();
+        vidaInimigo.text = inimigo.GetVida().ToString();
+        nomePlayer = GameObject.Find("NomePlayer").GetComponent<TextMeshProUGUI>();
+        nomePlayer.text = player.GetNomePersonagem();
+        nomeInimigo = GameObject.Find("NomeInimigo").GetComponent<TextMeshProUGUI>();
+        nomeInimigo.text = inimigo.GetNomePersonagem();
+        manaPlayer = GameObject.Find("ManaPlayer").GetComponent<Slider>();
+        manaPlayer.maxValue = player.GetMana();
+        manaPlayer.value = player.GetMana();
+        manaInimigo = GameObject.Find("ManaInimigo").GetComponent<Slider>();
+        manaInimigo.maxValue = inimigo.GetMana();
+        manaInimigo.value = inimigo.GetMana();
+        indicadorEspecial = GameObject.Find("IndicadorEspecial").GetComponentInChildren<TextMeshProUGUI>();
+        indicadorEspecial.text = player.ValorEspecial().ToString();
+        indicadorTempo = GameObject.Find("IndicadorTempo").GetComponent<TextMeshProUGUI>();
+        indicadorTempo.text = tempoRoundPlayer.ToString();
+        botaoEspecial.interactable = false;
+        botaoDefesa.interactable = false;
+        DefinirCorBotaoDesabilitado();
+        contadorCoroutine = StartCoroutine(ContadorRoundPlayer());
+    }
+
+    void Update()
+    {
+        AtualizaDadosTela();
+
+        if(turno == "Player" && verificadorDeTurno && player.VerificaVida())
+        {
+            botaoAtaque.interactable = true;
+            botaoDefesa.interactable = true;
+
+            if (player.VerificaEspecial())
+            {
+                botaoEspecial.interactable = true;
+            }
+            else
+            {
+                botaoEspecial.interactable = false;
+            }
+
+            verificadorDeTurno = false;
+        }
+        else if (turno == "Inimigo" && verificadorDeTurno && inimigo.VerificaVida())
+        {
+            StartCoroutine(AtaqueInimigo());
+        }
+
+        VerificaVitoria();
+    }
+
+    private void DefinirCorBotaoDesabilitado()
+    {
+        // Acessa o ColorBlock do botão
+        ColorBlock ca = botaoAtaque.colors;
+        ColorBlock ce = botaoEspecial.colors;
+        ColorBlock cd = botaoDefesa.colors;
+
+        // Altera a cor para o estado desabilitado
+        ca.disabledColor = new Color(0f, 0f, 0f, 0.5f);
+        ce.disabledColor = new Color(0f, 0f, 0f, 0.5f);
+        cd.disabledColor = new Color(0f, 0f, 0f, 0.5f);
+
+        // Aplica de volta ao botão
+        botaoAtaque.colors = ca;
+        botaoEspecial.colors = ce;
+        botaoDefesa.colors = cd;
+    }
+    public void AtaqueNormalPlayer()
+    {
+        inimigo.LevarDano(player.AtaqueNormal());
+        StartCoroutine(AtaquePlayer());
+    }
+
+    public void AtaqueEspecial()
+    {
+        inimigo.LevarDano(player.Especial());
+        StartCoroutine(AtaquePlayer());
+    }
+
+    public void DefesaEsquiva()
+    {
+        player.DefesaEsquiva();
+        StartCoroutine(DefesaPlayer());
+    }
+
+    private void AtualizaDadosTela()
+    {
+        vidaPlayer.text = player.GetVida().ToString();
+        vidaInimigo.text = inimigo.GetVida().ToString();
+        manaPlayer.value = player.GetMana();
+        manaInimigo.value = inimigo.GetMana();
+    }
+
+    public void RecebeTexto(string texto)
+    {
+        StartCoroutine(ExibeTexto(texto));
+    }
+
+    private IEnumerator ContadorRoundPlayer()
+    {
+        Debug.Log("Contador Iniciado");
+
+        contador = tempoRoundPlayer;
+
+        if (turno == "Player" && verificadorDeTurno)
+        {
+            while (verificadorDoContador && contador > 0)
+            {
+                yield return new WaitForSeconds(1f);
+                contador--;
+                indicadorTempo.text = contador.ToString();
+                Debug.Log($"Contador: {contador}");
+            }
+
+            if (contador <= 0)
+            {
+                informativo.text = "Tempo esgotado!";
+                StartCoroutine(AtaquePlayer());
+            }
+        }
+    }
+
+    private void StopContador()
+    {
+        if (contadorCoroutine != null)
+        {
+            StopCoroutine(contadorCoroutine);
+            contadorCoroutine = null;
+        }
+        verificadorDoContador = false;
+        indicadorTempo.text = "20";
+    }
+
+    private IEnumerator ExibeTexto(string texto)
+    {
+        informativo.text += texto + "\n";
+        yield return new WaitForSeconds(3f);
+        informativo.text = "";
+    }
+
+    private IEnumerator AtaqueInimigo()
+    {
+        StopContador();
+        verificadorDeTurno = false;
+
+        if (turno == "Inimigo")
+        {
+            botaoAtaque.interactable = false;
+            botaoEspecial.interactable = false;
+            player.LevarDano(inimigo.AtaqueNormal());
+            yield return new WaitForSeconds(3f);
+            verificadorDoContador = true;
+            verificadorDeTurno = true;
+            turno = "Player";
+            contadorCoroutine = StartCoroutine(ContadorRoundPlayer());
+        }
+    }
+
+    private IEnumerator AtaquePlayer()
+    {
+        StopContador();
+        verificadorDeTurno = false;
+        botaoAtaque.interactable = false;
+        botaoEspecial.interactable = false;
+        botaoDefesa.interactable = false;
+        indicadorEspecial.text = player.ValorEspecial().ToString();
+
+        if (turno == "Player")
+        {
+            yield return new WaitForSeconds(3f);
+            verificadorDeTurno = true;
+            turno = "Inimigo";
+        }
+    }
+
+    private IEnumerator DefesaPlayer()
+    {
+        StopContador();
+        verificadorDeTurno = false;
+        botaoAtaque.interactable = false;
+        botaoEspecial.interactable = false;
+        botaoDefesa.interactable = false;
+        indicadorEspecial.text = player.ValorEspecial().ToString();
+
+        if (turno == "Player")
+        {
+            yield return new WaitForSeconds(3f);
+            verificadorDeTurno = true;
+            turno = "Inimigo";
+        }
+    }
+
+    public void VerificaVitoria()
+    {
+
+        if (!inimigo.VerificaVida())
+        {
+            StartCoroutine(TelaVitoria());
+        }
+        else if (!player.VerificaVida())
+        {
+            StartCoroutine(TelaMorte());
+        }
+    }
+
+    IEnumerator TelaVitoria()
+    {
+        yield return new WaitForSeconds(1.0f);
+        player.PlaySomVitoria();
+        SceneManager.LoadScene("Vitoria");
+    }
+
+    IEnumerator TelaMorte()
+    {
+        yield return new WaitForSeconds(1.0f);
+        player.PlaySomMorte();
+        SceneManager.LoadScene("Derrota");
+    }
+
+    
+}
---
+++ Assets/Scripts/Mago.cs
@@ -0,0 +1,356 @@
+using System.Collections;
+using UnityEngine;
+
+public class Mago : MonoBehaviour
+{
+    [SerializeField] private string nomePersonagem;
+    [SerializeField] private int vida;
+    [SerializeField] private int ataque;
+    [SerializeField] private int defesa;
+    [SerializeField] private int especial;
+    [SerializeField] private int mana;
+    [SerializeField] private bool estahVivo = true;
+    [SerializeField] private DiretorBatalha dB;
+    [SerializeField] private Sprite spriteDerrota;
+    [SerializeField] private bool ehHeroi;
+    [SerializeField] private GameObject pDesefa;
+    [SerializeField] private GameObject pSangrar;
+    [SerializeField] private AudioClip[] somAtaque;
+    [SerializeField] private AudioClip[] somDefesa;
+    [SerializeField] private AudioClip[] somEspecial;
+    [SerializeField] private AudioClip[] somErroAtaque;
+    [SerializeField] private AudioClip[] somDano;
+    [SerializeField] private AudioClip somVitoria;
+    [SerializeField] private AudioClip somMorte;
+    [SerializeField] private AudioClip somEspecialPronto;
+    [SerializeField] private GameObject cameraC;
+
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
+    private SoundPlayer audioSource;
+    private FalasPersonagem falasPersonagem;
+    private bool defesaAtiva;
+    private int defesaTemp;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GameObject.FindGameObjectWithTag("Audio").GetComponent<SoundPlayer>();
+
+        //Procura a camera
+        if (cameraC == null)
+        {
+            cameraC = GameObject.FindGameObjectWithTag("MainCamera");
+        }
+
+        if (!falasPersonagem && !ehHeroi)
+        {
+            falasPersonagem = GetComponent<FalasPersonagem>();
+        }
+    }
+
+    public void FalaDoPersonagem(string tipoDeFala)
+    {
+        if (falasPersonagem != null && !ehHeroi && tipoDeFala == "Ataque")
+        {
+            falasPersonagem.FalaDeAtaque();
+        }
+        else if (falasPersonagem != null && !ehHeroi && tipoDeFala == "Defesa")
+        {
+            falasPersonagem.FalaDeDefesa();
+        }
+    }
+
+    public string GetNomePersonagem()
+    {
+        return nomePersonagem;
+    }
+
+    public int GetVida()
+    {
+        return vida;
+    }
+
+    public bool VerificaVida() //retorna true se o jogador esta vivo e false se esta morto
+    {
+        return estahVivo;
+    }
+
+    public bool VerificaEspecial() //retorna true se o jogador tem especial e false se nao tem
+    {
+        if (especial >= 3)
+        {
+            dB.RecebeTexto($"{nomePersonagem} especial pronto!");
+            audioSource.PlaySound(somEspecialPronto);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int ValorEspecial()
+    {
+        return especial;
+    }
+
+    public int GetMana()
+    {
+        return mana;
+    }
+
+    private void UseMana(int manaGasto)
+    {
+        mana -= manaGasto;
+    }
+
+    public void RegeneraMana(int manaGanho)
+    {
+        mana += manaGanho;
+    }
+
+    public int AtaqueNormal()
+    {
+        int valorAtaque = Random.Range(0, ataque);
+        UseMana(valorAtaque / 2);
+
+        especial++;
+
+        AnimaAtaque();
+
+        if (valorAtaque > 0)
+        {
+            FalaDoPersonagem("Ataque");
+            dB.RecebeTexto($"{nomePersonagem} ataca com {valorAtaque}");
+            PlaySomAtaque();
+        }
+        else
+        {
+            dB.RecebeTexto($"{nomePersonagem} erra o ataque.");
+            PlaySomErroAtaque();
+        }
+
+
+        return valorAtaque;
+    }
+
+    public int DefesaEsquiva()
+    {
+        int valorDefesa = Random.Range(0, defesa + (defesa / 2));
+        UseMana(valorDefesa / 2);
+        defesaTemp = valorDefesa;
+        defesaAtiva = true;
+
+        if (valorDefesa > 0)
+        {
+            FalaDoPersonagem("Defesa");
+            dB.RecebeTexto($"{nomePersonagem} defende com {valorDefesa}");
+        }
+        else
+        {
+            dB.RecebeTexto($"{nomePersonagem} nao consegue defender.");
+        }
+
+
+        return valorDefesa;
+    }
+
+    public bool DefesaAtiva()
+    {
+        if(defesaAtiva)
+        {
+            defesaAtiva = false;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public int Especial()
+    {
+        int valorEspecial = Random.Range((int)Mathf.Floor(ataque * 0.2f), ataque + (int)Mathf.Floor(ataque * 0.3f));
+        int chanceDeDobrar = Random.Range(0, 100);
+        int fatorMultiplicador = especial;
+
+        AnimaAtaque();
+
+        if (chanceDeDobrar >= 90 && especial >= 3 && mana == 100)
+        {
+            int valorEspecialDobrado = (valorEspecial * 2) + fatorMultiplicador;
+            FalaDoPersonagem("Ataque");
+            dB.RecebeTexto($"{nomePersonagem} ataca com {valorEspecialDobrado}");
+            PlaySomEspecial();
+            especial = 0;
+            UseMana(100);
+            return valorEspecialDobrado;
+        }
+        else if (chanceDeDobrar < 90 && especial >= 3 && mana >= valorEspecial)
+        {
+            FalaDoPersonagem("Ataque");
+            dB.RecebeTexto($"{nomePersonagem} ataca com {valorEspecial}");
+            PlaySomAtaque();
+            especial = 0;
+            UseMana(valorEspecial);
+            return valorEspecial;
+        }
+        else
+        {
+            dB.RecebeTexto("Seu especial não esta carregado!");
+            return 0;
+        }
+    }
+
+    public void LevarDano(int dano)
+    { 
+        int danoFinal;
+
+        if (defesaAtiva)
+        {
+            danoFinal = dano - defesaTemp;
+            defesaAtiva = false;
+        }
+        else
+        {
+            danoFinal = dano - DefesaEsquiva();
+        }
+           
+         
+        if (danoFinal <= 0)
+        {
+            RegeneraMana(danoFinal / 4);
+            StartCoroutine(TocarDefesa());
+        }
+        else if (danoFinal <= 25)
+        {
+            StartCoroutine(TocarDanoNormal(danoFinal));
+            RegeneraMana(danoFinal / 2);
+        }
+        else
+        {
+            StartCoroutine(TocarDanoMaximo(danoFinal));
+            RegeneraMana(danoFinal);
+        }
+
+        if (estahVivo)
+        {
+            Debug.Log($"{nomePersonagem}, vida: {vida}");
+        }
+        else
+        {
+            dB.RecebeTexto($"{nomePersonagem}, morreu!");
+        }
+
+    }
+    
+    private void DefineVida() //Verifica o valor da vida e define como morto
+    {
+        if (vida <= 0)
+        {
+            spriteRenderer.sprite = spriteDerrota;
+            vida = 0;
+            estahVivo = false; //Ta morto
+        }
+    }
+
+    private void AnimaAtaque()
+    {
+        if (ehHeroi)
+        {
+            anim.SetTrigger("AtaqueInimigo");
+        }
+        else
+        {
+            anim.SetTrigger("AtaqueHeroi");
+        }
+    }
+
+    private void PlaySomDano()
+    {
+        int som = Random.Range(0, somDano.Length);
+        audioSource.PlaySound(somDano[som]);
+    }
+
+    private void ParticulaDefesa()
+    {
+        pDesefa.GetComponent<ParticleSystem>().Play();
+    }
+
+    private void ParticulaSangrar()
+    {
+        pSangrar.GetComponent<ParticleSystem>().Play();
+    }
+
+    private void PlaySomAtaque()
+    {
+        int som = Random.Range(0, somAtaque.Length);
+        audioSource.PlaySound(somAtaque[som]);
+    }
+    private void PlaySomErroAtaque()
+    {
+        int som = Random.Range(0, somErroAtaque.Length);
+        audioSource.PlaySound(somErroAtaque[som]);
+    }
+
+    private void PlaySomDefesa()
+    {
+        int som = Random.Range(0, somDefesa.Length);
+        audioSource.PlaySound(somDefesa[som]);
+    }
+
+    private void PlaySomEspecial()
+    {
+        int som = Random.Range(0, somEspecial.Length);
+        audioSource.PlaySound(somEspecial[som]);
+    }
+
+    public void PlaySomMorte()
+    {
+        audioSource.PlaySound(somMorte);
+    }
+
+    public void PlaySomVitoria()
+    {
+        audioSource.PlaySound(somVitoria);
+    }
+
+    IEnumerator TocarDefesa()
+    {
+        dB.RecebeTexto($"{nomePersonagem} consegue se defender!");
+        anim.SetTrigger("Defesa");
+        yield return new WaitForSeconds(0.5f);
+        PlaySomDefesa();
+        ParticulaDefesa();
+    }
+
+    IEnumerator TocarDanoNormal(int danoFinal)
+    {
+        dB.RecebeTexto($"{nomePersonagem} leva dano de {danoFinal}.");
+        anim.SetTrigger("Dano");
+        yield return new WaitForSeconds(0.5f);
+        PlaySomDano();
+        ParticulaSangrar();
+        vida -= danoFinal; //vida = vida - danoFinal;
+        DefineVida();
+    }
+
+    IEnumerator TocarDanoMaximo(int danoFinal)
+    {
+        dB.RecebeTexto($"{nomePersonagem} toma uma porrada de {danoFinal}.");
+        anim.SetTrigger("Dano");
+        yield return new WaitForSeconds(0.5f);
+        CameraTreme(0.5f);
+        PlaySomDano();
+        ParticulaSangrar();
+        vida -= danoFinal;
+        DefineVida();
+    }
+
+    private void CameraTreme(float magnitude)
+    {
+        audioSource.PlaySound(somVitoria);
+        cameraC.GetComponent<CameraShake>().ShakeCamera(0.5f, magnitude);
+    }
+}
